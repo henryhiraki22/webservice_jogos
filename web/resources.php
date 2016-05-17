99da2abe89dbb9077ffdc843a4697df380c2a019
henryhiraki22 <henry.hiraki@gmail.com>
2016-05-17 18:17:53 +0000

    Adicionado o resources.php

--- web/resources.php
+++ web/resources.php
@@ -35,5 +35,42 @@ class GeneralResourceGET extends GeneralResource{
         }
     }
 }
+
+class GeneralResourceOPTIONS extends GeneralResource{
+    
+    public function buscaJogo(){
+            header('allow: GET, OPTIONS');
+            http_response_code(200); 
+        }
+    
+    public function getJogo(){
+            header('allow: POST, OPTIONS');
+            http_response_code(200);
+            //nao sei se isso está certo.
+        }        
+}
+
+class GeneralResourcePOST extends GeneralResource{
+    
+    public function insereJogo(){
+        if($_SERVER["CONTENT_TYPE"] === "application/json"){
+            $json = file_get_contents('php://input');
+            $array = json_decode($json,true);
+            require_once "../model/jogo.php";
+            require_once "../model/jogoDAO.php";
+            $jogo = new Jogo(0,$array["nome"],$array["valor"],$array["plataforma"], $array["genero"]);
+            //array que retorna as informações, caso adicionar outra coisa relacionada ao jogo, adicionar aki tbm!
+            $jd = new JogoDAO();
+            $jd->insert($jogo);
+            echo json_encode(array("response"=>"Criado"));
+            http_response_code(500);   
+        }else{
+            echo json_encode(array("response"=>"Dados inválidos"));
+            http_response_code(500);   
+        }
+    }
+
+
+}
     
 ?>    
