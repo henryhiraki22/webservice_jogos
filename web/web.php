2fa303d44a1f117a0efc8e9a951c84d3ec5f4670
henryhiraki22 <henry.hiraki@gmail.com>
2016-05-17 18:39:06 +0000

    add a classe controller.

---
+++ web/web.php
@@ -0,0 +1,28 @@
+<?php
+
+class Controller{
+    //VEM DA URL
+    private $met;
+    
+    public function __construct($met){
+        $this->met = $met;
+    }
+    
+    public function gerenciar(){
+        require_once "resources.php";
+        $rm = $_SERVER["REQUEST_METHOD"];
+        $clazz = "GeneralResource" . $rm;
+        $resource = new $clazz();
+        $nomeDoMetodo = $this->met;
+        $resource->$nomeDoMetodo();
+    }
+}
+
+$met = $_POST["metodo"];
+$class = $_POST["classe"];
+$met = $_GET["metodo"];
+$class = $_GET["classe"];
+$r = new Controller($met);
+$r->gerenciar();
+
+?>
