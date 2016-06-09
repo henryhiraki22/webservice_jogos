28ad2b200a9c234bf27f375eecc254e92a53f3ae
henryhiraki22 <henry.hiraki@gmail.com>
2016-06-09 11:57:47 -0300

    Create livro.php

---
+++ model/livro.php
@@ -0,0 +1,27 @@
+<?php
+
+class Livro{
+    
+    private $id, $nome, $valor, $genero;
+    
+    public function __construct($id,$nome,$valor,$genero){
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->valor = $valor;
+        $this->genero = $genero;
+    }   
+    public function getId(){
+        return $this->id;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getValor(){
+        return $this->valor;
+    }
+    public function getGenero(){
+        return $this->genero;
+    }
+}
+
+?>
