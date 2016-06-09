290524b65e2a87c49b3b713662c2c2b08c91487e
henryhiraki22 <henry.hiraki@gmail.com>
2016-06-09 05:59:58 -0300

    Serie.php criado.

---
+++ model/serie.php
@@ -0,0 +1,28 @@
+<?php
+
+class Serie{
+    
+    private $id, $nome, $valor, $genero;
+    
+    public function __construct($id,$nome,$valor,$genero){
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->valor = $valor;
+        $this->genero = $genero;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getValor(){
+        return $this->valor;
+    }
+  
+    public function getGenero(){
+        return $this->genero;
+    }
+}
+
+?>
